5a354d1c6cc3ee404a0047fac93ccfbaa27ffedf
theambrosial <vikas.pandey9323@gmail.com>
2020-12-30 20:37:55 +0530

    initial commit

---
+++ nature/web/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.core.mail import EmailMessage, send_mail
+# Create your views here.
+from nature import settings
+from django.contrib import messages
+
+def home(request):
+    if request.method == 'POST' or request.method == 'FILES':
+        contact_name = request.POST.get('contact_name')
+        contact_email = request.POST.get('contact_email')
+        contact_number = request.POST.get('contact_number')
+        contact_msg = request.POST.get('contact_address')
+        city = request.POST.get('city')
+        pin_code = request.POST.get('pin_code')
+
+        msg = '''
+        Below are form details:
+        
+        Name : '''+contact_name+'''\n
+        Email : '''+contact_email+'''\n
+        Mobile : '''+contact_number+'''\n
+        City : '''+city+'''\n
+        Pin Code : '''+pin_code+'''\n
+        Address : '''+contact_msg+'''\n
+        
+        '''
+
+        try:
+
+            send_mail("New Form - Nature's Blessing", msg, settings.EMAIL_HOST_USER, ['thenaturesblessing@gmail.com',])
+            print('email sent')
+            messages.success(request, "Submitted Successfully!!!")
+        except:
+            print('exception occured email not sent')
+            messages.error(request, "Error in submitting form, Try Again after some time.")
+    return render(request,'index.html')
